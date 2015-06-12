a83ae861da89a173a7318f821f368abdc5f6d241
Brian Levinstein <blevinstein@gmail.com>
2015-06-12 01:05:52 -0700

    Add broken rapidity test in VelocityTest.

--- test/com/blevinstein/sr/VelocityTest.java
+++ test/com/blevinstein/sr/VelocityTest.java
@@ -15,8 +15,15 @@ public class VelocityTest {
   @Test
   public void angleTo() {
     Velocity v1 = new Velocity(10, 20);
-    Velocity v2 = new Velocity(-5, 50);
+    Velocity v2 = new Velocity(-5, 25);
 
     Assert.assertEquals(Math.abs(v1.angle() - v2.angle()), v1.angleTo(v2), 0.01);
+  }
+
+  //@Test BROKEN
+  //TODO: Fix. Either my math or my understanding of SR is incorrect.
+  public void rapidity() {
+    Velocity v = new Velocity(25, 0);
+    Assert.assertEquals(v.rapidity() * 2, v.relativePlus(v).rapidity(), 0.001);
   }
 }
