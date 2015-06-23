cd08a5c7ae422f3213c98f201f18da135cc5c6f7
Brian Levinstein <blevinstein@gmail.com>
2015-06-23 01:55:08 -0700

    CircularTimeline -> EllipticalTimeline in SolarSystemGalaxy.

--- src/com/blevinstein/sr/asteroids/SRAsteroids.java
+++ src/com/blevinstein/sr/asteroids/SRAsteroids.java
@@ -50,8 +50,8 @@ public class SRAsteroids {
   public void reset() {
     //galaxy = new UniformBubbleGalaxy(1E4, 1E-5);
     //galaxy = new CircleGalaxy(1E4, 5E-6, 1E7);
-    //galaxy = new SolarSystemGalaxy(1E4, 10 /* planets */, 5 /* moons */, 1E7);
-    galaxy = new GridGalaxy(1E4, 1E4, 100, 100, 10);
+    galaxy = new SolarSystemGalaxy(1E4, 10 /* planets */, 5 /* moons */, 1E7);
+    //galaxy = new GridGalaxy(1E4, 1E4, 100, 100, 10);
 
     observer = new Event(1E3 * (Math.random() - 0.5), 1E3 * (Math.random() - 0.5), 0);
     velocity = Velocity.ZERO;
--- src/com/blevinstein/sr/asteroids/SolarSystemGalaxy.java
+++ src/com/blevinstein/sr/asteroids/SolarSystemGalaxy.java
@@ -3,7 +3,7 @@ package com.blevinstein.sr.asteroids;
 import static com.blevinstein.sr.SR.c;
 import static com.blevinstein.util.Prob.poisson;
 
-import com.blevinstein.sr.CircularTimeline;
+import com.blevinstein.sr.EllipticalTimeline;
 import com.blevinstein.sr.Event;
 import com.blevinstein.sr.StaticTimeline;
 import com.blevinstein.sr.Timeline;
@@ -28,23 +28,29 @@ public class SolarSystemGalaxy implements Galaxy {
 
     int numPlanets = poisson(expPlanets);
     for (int i = 0; i < numPlanets; i++) {
-      CircularTimeline planet = randomOrbit(sun, maxRadius, gravity);
+      EllipticalTimeline planet = randomOrbit(sun, maxRadius, gravity);
       _stars.add(new Star(planet, randomColor(), 10 /* radius */, 0 /* no twinkle */));
       
       int numMoons = poisson(expMoons);
       for (int j = 0; j < numMoons; j++) {
-        CircularTimeline moon = randomOrbit(planet, maxRadius/numPlanets, gravity/numPlanets);
+        EllipticalTimeline moon = randomOrbit(planet, maxRadius/numPlanets, gravity/numPlanets);
         _stars.add(new Star(moon, randomColor(), 5 /* radius */, 0 /* no twinkle */));
       }
     }
   }
 
-  private CircularTimeline randomOrbit(Timeline center, double maxRadius, double gravity) {
+  private EllipticalTimeline randomOrbit(Timeline center, double maxRadius, double gravity) {
     double angle = Math.random() * 2 * Math.PI;
-    double minRadius = gravity / (c * c); // so that velocity < c
-    double radius = Math.sqrt(Math.random()) * (maxRadius - minRadius) + minRadius;
-    double velocity = Math.sqrt(gravity / radius);
-    return new CircularTimeline(center, radius, velocity, angle);
+    double eccentricity = Math.random() * EllipticalTimeline.MAX_ECCENTRICITY;
+    double maxMajorAxis = maxRadius / (1 + eccentricity);
+    double E = 2 / (1 - eccentricity) - 1;
+    double minMajorAxis = gravity * E / (c * c);
+    if (minMajorAxis > maxMajorAxis) {
+      System.out.println("randomOrbit abort");
+      return randomOrbit(center, maxRadius, gravity);
+    }
+    double majorAxis = Math.sqrt(Math.random()) * (maxMajorAxis - minMajorAxis) + minMajorAxis;
+    return new EllipticalTimeline(angle, center, eccentricity, gravity, majorAxis);
   }
 
   private Color randomColor() {
